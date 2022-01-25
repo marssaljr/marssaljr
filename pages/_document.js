1ccd2f6fb8464da86fd71bce89cb1d9f5dc8b55f
Marçal Junior <marssaljr@gmail.com>
2022-01-25 02:49:16 -0300

    fix: console error

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Slab:wght@200;300;400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
