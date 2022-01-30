481644eb86f174345e5e43005e3d30a2ff5a38ed
Marçal Junior <marssaljr@gmail.com>
2022-01-30 16:52:59 -0300

    fix: form name

--- pages/hire.js
+++ pages/hire.js
@@ -24,7 +24,12 @@ export default function Hire() {
           <h1>Send a mail to me</h1>
           <p>It's free</p>
         </div>
-        <form className={styles.form} method="POST" data-netlify="true">
+        <form
+          name="contact"
+          className={styles.form}
+          method="POST"
+          data-netlify="true"
+        >
           <input type="hidden" name="form-name" value="contact" />
           <input
             type="text"
